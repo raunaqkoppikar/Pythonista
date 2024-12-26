32f14daa4bd88b10538d48c88e7a9900329f1200
Raunaq Gautam Koppikar <rgvk25@gmail.com>
2024-12-26 12:50:15 +0530

    Dictionary of squared numbers

---
+++ Practice/squared_dict.py
@@ -0,0 +1,6 @@
+def squared_dict(numbers):
+    # Your code here
+    return {number: number ** 2 for number in numbers}
+
+# Test
+print(squared_dict([1, 2, 3, 4]))  # Output: {1: 1, 2: 4, 3: 9, 4: 16}
